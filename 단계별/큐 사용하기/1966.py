e7320232b3489b5264e255bf66bcc70a55c4f53f
P00HP00H <k5602o@nate.com>
2019-02-23 14:03:04 +0900

    해당 숫자와 index 따로 구별해야겠다는 아이디어 생각

--- 단계별/큐 사용하기/1966.py
+++ 단계별/큐 사용하기/1966.py
@@ -5,6 +5,8 @@ for i in range(0, a):
     c = int(input())
     for j in range(0, b):
         d = int(input())
+    # 출력하기를 원하는 수 저장
+    e = d[c]
     for k in range(0, b-1):
         for l in range(k+1, b):
             if d[k] <= d[l]:
