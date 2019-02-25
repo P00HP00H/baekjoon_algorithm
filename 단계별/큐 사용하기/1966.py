0af260f712b78fadedccd794182e8acefbb9688f
P00HP00H <k5602o@nate.com>
2019-02-25 17:19:08 +0900

    거의 완성 - 예제2번만 성공 1,3번은 실패

--- 단계별/큐 사용하기/1966.py
+++ 단계별/큐 사용하기/1966.py
@@ -1,17 +1,37 @@
 a = int(input())
+que_chk = []
 
 for i in range(0, a):
-    b = int(input())
-    c = int(input())
-    for j in range(0, b):
-        d = int(input())
-    # 출력하기를 원하는 수 저장
-    e = d[c]
-    for k in range(0, b-1):
-        for l in range(k+1, b):
-            if d[k] <= d[l]:
-                d.append(d[0])
-                d.remove(d[0])
+    b, c = input().split()
+    for j in range(0, int(b)):
+#IndexError: list assignment index out of range
+        que_chk.append(0)
+    que_chk[int(c)] = 1
+    d = input().split()
+
+
+    for l in range(0, int(b)):
+        #print('for문값')
+        #print(l)
+        #print(d)
+        #print(que_chk)
+        while 1:
+            for k in range(l+1, int(b)):
+                if int(d[l]) <= int(d[k]):
+                    d.append(d[l])
+                    que_chk.append(que_chk[l])
+#                    print('zzz')
+#                    print(d)
+#                    print(que_chk)
+                    del d[l]
+                    del que_chk[l]
+#                    print('zzz2')
+#                    print(d)
+#                    print(que_chk)
+                    break
+            if k == int(b)-1:
                 break
-    d.pop()
-    # 해야할 일 : 몇번째 출력하기를 원하는지에 대해 코딩하기
+    for m in range(0, int(b)):
+        if que_chk[m] == 1:
+            print(m+1)
+
