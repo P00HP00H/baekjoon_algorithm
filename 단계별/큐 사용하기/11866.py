d5f5e000dfb6d2b206d931b91b6f96eb405f400d
P00HP00H <k5602o@nate.com>
2019-03-07 18:18:46 +0900

    백준 11866번 정답, 설명 추가 덜 됨

--- 단계별/큐 사용하기/11866.py
+++ 단계별/큐 사용하기/11866.py
@@ -1,15 +1,17 @@
-a, b = map(int, input().split())
-que = []
-output = []
+a, b = map(int, input().split())    # 입력 하나에 여러값을 입력하면 값을 분리할 때 각각은 str 형태이다. a,b에 일일이 int를 계속 붙여주기가 귀찮아 map 함수를 이용해 미리 int형으로 쪼개지도록 해줌
+que = []        # 큐
+output = []     # 출력값
 
-for i in range(0, a):
+for i in range(0, a):       # 1부터 a까지 que에 저장
     que.append(i+1)
 
-for j in range(0, a-b+2):
-    if j == a-b+1:
-        for k in range(0, b-1):
-            output.append(que[0])
+for j in range(0, a):       # 사람이 없어질때까지 반복하므로 a번
+    if len(que) < b:        # b번째를 제거하는데, 사람수가 b보다 작을 때
+        for k in range(0, b-len(que)):
+            que.append(que[0])
             que.pop(0)
+        output.append(que[-1])
+        que.pop(-1)
     else:
         output.append(que[b-1])
         que.pop(b-1)
@@ -23,4 +25,40 @@ for m in range(0, a):
         print(output[m], end = ">")
     else:
         print(output[m], end = ", ")
+
+
+
+# 풀이 2
+
+a, b = map(int, input().split())
+que = []
+output = []
+
+for i in range(0, a):
+    que.append(i+1)
+
+while 1:
+    if len(que) == 0:
+        break
+    elif len(que) < b:
+        for k in range(0, b-len(que)):
+            que.append(que[0])
+            que.pop(0)
+        output.append(que[-1])
+        que.pop(-1)
+    else:
+        output.append(que[b-1])
+        que.pop(b-1)
+        for l in range(0, b-1):
+            que.append(que[0])
+            que.pop(0)
+
+print("<", end = "")
+for m in range(0, a):
+    if m == a-1:
+        print(output[m], end = ">")
+    else:
+        print(output[m], end = ", ")
+
+
 
