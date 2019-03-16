2a23b789fc08117a4deadbdb21bc1192ad98abea
P00HP00H <k5602o@nate.com>
2019-03-16 15:42:26 +0900

    2가지 방법으로 풀어봤는데도 둘 다 시간초과

--- 단계별/덱 사용하기/5430.py
+++ 단계별/덱 사용하기/5430.py
@@ -1,3 +1,4 @@
+# 잘못된 풀이
 a = int(input())        # 1번째 입력값
 chk = []
 reverse = []
@@ -18,20 +19,21 @@ for i in range(0, a):
     else:
         if len(e) > 1:
             e = d[1:-1].split(',')
+            f = list(map(int, e))
 
         num = 0
         for k in range(0, len(chk)):
             if chk[k] == "D":
-                if len(e) == 0:
+                if len(f) == 0:
                     output.append('Error')
                     num += 1
                     break
                 else:
-                    e.pop(0)
+                    f.pop(0)
             elif chk[k] == "R":
-                for l in range(0, len(e)):
-                    reverse.append(max(e))
-                    e.pop(e.index(max(e)))
+                for l in range(0, len(f)):
+                    reverse.append(max(f))
+                    e.pop(e.index(max(f)))
                 e = reverse
 
         if num == 0:
@@ -44,14 +46,14 @@ for m in range(0, len(output)):
 
 
 
-
+# reverse() 쓴 풀이
 a = int(input())        # 1번째 입력값
 chk = []
 reverse = []
 output = []
 
 for i in range(0, a):
-    b = input()         # 2번째 입력값
+    b = input()     # 2번째 입력값
     for j in range(0, len(b)):
         chk.append(b[j:j+1])
 
@@ -80,7 +82,60 @@ for i in range(0, a):
 
         if num == 0:
             output.append(f)
+    chk = []
 
 # 출력
 for m in range(0, len(output)):
     print(output[m])
+
+
+
+
+# 내가 만든 풀이
+a = int(input())
+chk = []
+reverse = []
+output = []
+
+for i in range(0, a):
+    b = input()         # 2번째 입력값
+    for j in range(0, len(b)):
+        chk.append(b[j:j+1])
+
+    c = int(input())        # 3번째 입력값
+    d = input()         # 4번째 입력값
+    e = d[1:-1]
+
+    if len(e) == 0:
+        output.append('Error')
+    else:
+        if len(e) > 1:
+            e = d[1:-1].split(',')
+            f = list(map(int, e))
+
+        num = 0
+        for k in range(0, len(chk)):
+            if chk[k] == "D":
+                if len(f) == 0:
+                    output.append('Error')
+                    num += 1
+                    break
+                else:
+                    f.pop(0)
+            elif chk[k] == "R":
+                for l in range(0, len(f)):
+                    reverse.append(f[-1])
+                    f.pop(-1)
+                f = reverse
+                reverse = []
+
+        if num == 0:
+            output.append(f)
+    chk = []
+    reverse = []
+
+# 출력
+for m in range(0, len(output)):
+    print(output[m])
+
+
