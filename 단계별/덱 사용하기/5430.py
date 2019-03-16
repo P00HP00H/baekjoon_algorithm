0970c29618c7b987d8ad83a424d48e6cfdfab329
P00HP00H <k5602o@nate.com>
2019-03-16 14:17:34 +0900

    전역 변수 잘못된듯 - 입력1개일때는 맞지만 여러개일때는 틀림

--- 단계별/덱 사용하기/5430.py
+++ 단계별/덱 사용하기/5430.py
@@ -40,3 +40,47 @@ for i in range(0, a):
 # 출력
 for m in range(0, len(output)):
     print(output[m])
+
+
+
+
+
+a = int(input())        # 1번째 입력값
+chk = []
+reverse = []
+output = []
+
+for i in range(0, a):
+    b = input()         # 2번째 입력값
+    for j in range(0, len(b)):
+        chk.append(b[j:j+1])
+
+    c = int(input())        # 3번째 입력값
+    d = input()         # 4번째 입력값
+    #e = d[1:-1].split(',')
+    e = d[1:-1]
+
+    if len(e) == 0:
+        output.append('Error')
+    else:
+        if len(e) > 1:
+            e = d[1:-1].split(',')
+            f = list(map(int, e))
+        num = 0
+        for k in range(0, len(chk)):
+            if chk[k] == "D":
+                if len(f) == 0:
+                    output.append('Error')
+                    num += 1
+                    break
+                else:
+                    f.pop(0)
+            elif chk[k] == "R":
+                f.reverse()
+
+        if num == 0:
+            output.append(f)
+
+# 출력
+for m in range(0, len(output)):
+    print(output[m])
