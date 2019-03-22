264505028d3631bd7c6702d0fc118e562a00bd49
P00HP00H <k5602o@nate.com>
2019-03-22 18:09:03 +0900

    코드를 돌려보면 맞는거 같은데 시간 초과 뜨거나 어떤 경우는 런타임에러 뜸 --> 포기

--- 단계별/덱 사용하기/5430.py
+++ 단계별/덱 사용하기/5430.py
@@ -49,7 +49,6 @@ for m in range(0, len(output)):
 # reverse() 쓴 풀이
 a = int(input())        # 1번째 입력값
 chk = []
-reverse = []
 output = []
 
 for i in range(0, a):
@@ -139,3 +138,118 @@ for m in range(0, len(output)):
     print(output[m])
 
 
+
+
+
+# reverse() 쓴 풀이
+a = int(input())
+output = []
+
+for i in range(0, a):
+    b = input()
+    c = int(input())
+    d = input()
+    e = d[1:-1]
+
+    if len(e) == 0:
+        output.append('Error')
+    else:
+        if len(e) > 1:
+            e = d[1:-1].split(',')
+            f = list(map(int, e))
+        num = 0
+        for k in range(0, len(b)):
+            if b[k] == "D":
+                if len(f) == 0:
+                    output.append('Error')
+                    num += 1
+                    break
+                else:
+                    f.pop(0)
+            elif b[k] == "R":
+                f.reverse()
+
+        if num == 0:
+            output.append(f)
+
+for m in range(0, len(output)):
+    print(output[m])
+
+
+
+
+########### 이 2개로
+
+a = int(input())
+
+for i in range(0, a):
+    b = input()
+    c = int(input())
+    d = input()
+    e = d[1:-1]
+
+    if len(e) == 0:
+        print('Error')
+    else:
+        e = d[1:-1].split(',')
+
+        num = 0
+        for k in range(0, len(b)):
+            if b[k] == "D":
+                if len(e) == 0:
+                    print('Error')
+                    num += 1
+                    break
+                else:
+                    e.pop(0)
+            elif b[k] == "R":
+                e.reverse()
+        if num == 0:
+            print("[", end = "")
+            for m in range(0, len(e)):
+                if m == len(e)-1:
+                    print(int(e[m]), end = "]")
+                    print("")
+                else:
+                    print(int(e[m]), end = ",")
+
+
+
+a = int(input())
+
+for i in range(0, a):
+    reverse = []
+    b = input()
+    c = int(input())
+    d = input()
+    e = d[1:-1]
+
+    if len(e) == 0:
+        print('Error')
+    else:
+        if len(e) > 1:
+            e = d[1:-1].split(',')
+        num = 0
+        for k in range(0, len(b)):
+            if b[k] == "D":
+                if len(e) == 0:
+                    print('Error')
+                    num += 1
+                    break
+                else:
+                    e.pop(0)
+            elif b[k] == "R":
+                for l in range(0, len(e)):
+                    reverse.append(e[-1])
+                    e.pop(-1)
+                e = reverse
+                reverse = []
+
+        if num == 0:
+            print("[", end = "")
+            for m in range(0, len(e)):
+                if m == len(e) - 1:
+                    print(int(e[m]), end = "]")
+                    print()
+                else:
+                    print(int(e[m]), end = ",")
