ee3e17cc7cc0c5aa3aaabf359fdbc54ffcba88d7
P00HP00H <k5602o@nate.com>
2019-04-01 17:33:15 +0900

    백준 1021번 - 회전하는 큐

--- 단계별/덱 사용하기/1021.py
+++ 단계별/덱 사용하기/1021.py
@@ -1,29 +1,30 @@
-que = []
-num = 0
-a, b = map(int, input().split())
-c = input().split()
+deque = []
+rotate_num = 0         # 회전 수
+a, b = map(int, input().split())    # 첫 번째 입력값
+c = input().split()                 # 두 번째 입력값
 
-for i in range(0, a):
-    que.append(i+1)
+for i in range(0, a):       # 1~a까지 입력
+    deque.append(i+1)
 
 for j in range(0, b):
-    if que.index(int(c[j])) <= len(que)/2:
+    if deque.index(int(c[j])) <= len(deque)/2:      # 뽑아내려는 숫자가 deque 중간 기준 왼쪽에 있는 경우 -> 맨 앞 숫자를 맨 뒤로 보냄
         while 1:
-            if que[0] == int(c[j]):
-                que.pop(0)
+            if deque[0] == int(c[j]):       # 뽑아내려는 숫자가 맨 앞에 있는 경우 뽑아내기
+                deque.pop(0)
                 break
-            else:
-                que.append(que[0])
-                que.pop(0)
-                num += 1
-    else:
+            else:           # 그렇지 않으면 숫자들 재배치(맨 앞 숫자를 맨 뒤로 보냄)
+                deque.append(deque[0])
+                deque.pop(0)
+                rotate_num += 1
+
+    else:           # 뽑아내려는 숫자가 deque 중간 기준 오른쪽에 있는 경우 -> 맨 뒤 숫자를 맨 앞으로 보냄
         while 1:
-            if que[0] == int(c[j]):
-                que.pop(0)
+            if deque[0] == int(c[j]):       # 뽑아내려는 숫자가 맨 앞에 있는 경우 뽑아내기
+                deque.pop(0)
                 break
-            else:
-                que.insert(0, que[-1])
-                que.pop(-1)
-                num += 1
+            else:           # 그렇지 않으면 숫자들 재배치(맨 뒤 숫자를 맨 앞으로 보냄)
+                deque.insert(0, deque[-1])
+                deque.pop(-1)
+                rotate_num += 1
 # 출력
-print(num)
+print(rotate_num)
