4cca5c0c74c493c09415679ac505e8695e48a20b
P00HP00H <k5602o@nate.com>
2019-04-09 03:17:00 +0900

    백준 2606번 - DFS 어느정도 완성

--- 단계별/그래프 (DFS, BFS)/2606.py
+++ 단계별/그래프 (DFS, BFS)/2606.py
@@ -1,29 +1,55 @@
-visit = []      # stack
+stack = []
 visited = []
 a = int(input())
-adj = [[0]*a for i in range(a)]
+adj = [[0]*(a+1) for i in range(a+1)]
 b = int(input())
 
 for i in range(0, b):
-    c = input().split()
+    c = list(map(int, input().split()))
     adj[c[0]][c[1]] = 1
     adj[c[1]][c[0]] = 1
-    while visit:
-        d = visit.pop()
+    while stack:
+        d = stack.pop()
         visited.append(d)
         for v in adj[d]:
-            if v not in visited+visit:
-                visit.append(v)
-    return visited
+            if v not in visited+stack:
+                stack.append(v)
+print(len(visited)-1)
+
+# 여기부터
+def DFS(adj,s):
+    stack = [s]
+    visited = []
+    while stack:
+        d = stack.pop()
+        visited.append(d)
+        for v in adj[d]:
+            if v not in visited+stack:
+                stack.append(v)
+    print(visited)
+
+a = int(input())
+adj = [[0]*(a+1) for i in range(a+1)]
+b = int(input())
+
+for i in range(0, b):
+    c = list(map(int, input().split()))
+    adj[c[0]][c[1]] = 1
+    adj[c[1]][c[0]] = 1
+
+DFS(adj, 1)
+
+
+
 
 
 def DFS(adj,s):
-    tovisit = [s]
+    stack = [s]
     visited = []
-    while tovisit:
-        u = tovisit.pop()
+    while stack:
+        u = stack.pop()
         visited.append(u)
         for v in adj[u]:
-            if v not in visited+tovisit:
-                tovisit.append(v)
+            if v not in visited+stack:
+                stack.append(v)
     return visited
