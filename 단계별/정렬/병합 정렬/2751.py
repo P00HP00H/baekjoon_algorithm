500da78e522a9d41b54b07dd6b07886f9b5dfdd3
P00HP00H <k5602o@nate.com>
2019-07-12 03:41:04 +0900

    백준 2751번 : 답은 맞음, 하지만 시간초과 -> 조금 더 보고 수정해보고 안되면 변수명, 코드 설명만 붙이고 끝내자

--- 단계별/정렬/병합 정렬/2751.py
+++ 단계별/정렬/병합 정렬/2751.py
@@ -1,4 +1,11 @@
-# 병합 정렬
+# 병합 정렬 - 시간 초과 뜸(다른 블로그들도 거의 다 시간초과)
+
+array = []
+a = int(input())        # 입력 갯수
+
+for l in range(0, a):
+    b = int(input())
+    array.append(b)     # 입력값들을 순서대로 배열에 저장
 
 def merge_sorted(arr):
     if len(arr) > 1:
@@ -13,34 +20,23 @@ def merge_sorted(arr):
         return arr
 
 def merge(left, right):
-    if len[left] == 1 and len[right] == 1:
+    sorted_array = []
+    while len(left) != 0 and len(right) != 0:
         if left[0] < right[0]:
             sorted_array.append(left[0])
-            sorted_array.append(right[0])
+            del left[0]
         else:
             sorted_array.append(right[0])
-            sorted_array.append(left[0])
+            del right[0]
 
-    else:       # 한 쪽 배열이 모두 없어졌을 경우 계산하는 코드를 넣어야 함 -> 여기 할 차례
-        for i in range(0, len(left)*len(right)):
-            if left[0] < right[0]:
-                sorted_array.append(left[0])
-                del left[0]
-            else:
-                sorted_array.append(right[0])
-                del right[0]
+    if len(left) == 0:
+        sorted_array += right
+    elif len(right) == 0:
+        sorted_array += left
 
     return sorted_array
 
-array = []
-sorted_array = []
-a = int(input())        # 입력 갯수
-
-for l in range(0, a):
-    b = int(input())
-    array.append(b)     # 입력값들을 순서대로 배열에 저장
-
-merge_sorted(array)
+kkk = merge_sorted(array)
 
 for i in range(0, len(array)):      # 출력
-    print(sorted_array[i])
+    print(kkk[i])
