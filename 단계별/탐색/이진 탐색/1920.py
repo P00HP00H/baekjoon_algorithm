e8bb2138e51352edaf156c3d8f8cd88bfdb4b6a1
P00HP00H <k5602o@nate.com>
2019-07-18 15:45:41 +0900

    백준 1920번 초기 설정

---
+++ 단계별/탐색/이진 탐색/1920.py
@@ -0,0 +1,11 @@
+# 이진 탐색
+
+a = int(input())        # 입력 갯수
+b = list(map(int, input().split()))
+
+c = int(input())
+d = list(map(int, input().split()))
+
+
+for i in range(0, c):
+    print('이진 탐색')
