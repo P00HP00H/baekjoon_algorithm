f4b2f481ad98ed0f8a6245983d3083733962307f
P00HP00H <k5602o@nate.com>
2019-08-09 06:25:46 +0900

    백준 1920번 - 왼쪽, 오른쪽 나눠서 처리

--- 단계별/탐색/이진 탐색/1920.py
+++ 단계별/탐색/이진 탐색/1920.py
@@ -10,11 +10,21 @@ b.sort()    # 이진 탐색은 정렬된 배열이어야만 가능
 
 def binary_search(arr_num, num):
     while(arr_num != 0):       # arr_num = 0일 때(즉, 끝까지 탐색)까지 무한반복
-        arr_num //= 2
-        if(num == b[arr_num]):
-            return 1
-        elif(arr_num == 0):
-            return 0
+        #arr_num //= 2
+        if(num < b[arr_num]): # 왼쪽 배열 처리
+            arr_num //= 2
+            if(num == b[arr_num]):
+                return 1
+            elif(arr_num == 0):
+                return 0
+        elif(num > b[arr_num]):
+            arr_num += len(b)
+            arr_num //= 2
+            if (num == b[arr_num]):
+                return 1
+            elif (arr_num == 0):
+                return 0
 
-for i in range(0, c):
-    print(binary_search(a, i))
+
+for i in range(0, d):
+    print(binary_search(a, d[i]))
